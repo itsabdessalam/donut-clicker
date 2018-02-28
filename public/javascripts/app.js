df3e704ab634cba85c9a837e43cc1546afaf803b
vmarlier <valentin.marlier@gmail.com>
2018-02-28 23:24:40 +0100

    [Added] donut transition onclick

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,33 +1,34 @@
 const socket = io();
 
 function updateAchievement() {
-    const nbDonuts = parseInt($('.nbDonuts').text());
-    const donutsPerSec = parseInt($('.donutsPerSec').text());
-    socket.emit('achievements', nbDonuts, donutsPerSec);
+  const nbDonuts = parseInt($(".nbDonuts").text());
+  const donutsPerSec = parseInt($(".donutsPerSec").text());
+  socket.emit("achievements", nbDonuts, donutsPerSec);
 }
 
 function addDonutsPerSecond() {
-    const nbDonuts = parseInt($('.nbDonuts').text());
-    const donutsPerSec = parseInt($('.donutsPerSec').text());
-    socket.emit('dPs', nbDonuts, donutsPerSec);
+  const nbDonuts = parseInt($(".nbDonuts").text());
+  const donutsPerSec = parseInt($(".donutsPerSec").text());
+  socket.emit("dPs", nbDonuts, donutsPerSec);
 }
 setInterval(addDonutsPerSecond, 1000);
 
-$('#donutLink').click((e) => {
-    e.preventDefault;
-    const nbDonuts = parseInt($('.nbDonuts').text());
-    socket.emit('AddDonut', nbDonuts);
+$("#donutLink").click(e => {
+  e.preventDefault;
+  const nbDonuts = parseInt($(".nbDonuts").text());
+  socket.emit("AddDonut", nbDonuts);
+  $("img").toggleClass("transition");
 });
 
-socket.on('GetDonuts', function (data) {
-    $('.nbDonuts').text(data);
-    updateAchievement();
+socket.on("GetDonuts", function(data) {
+  $(".nbDonuts").text(data);
+  updateAchievement();
 });
 
-socket.on('toast', (data) => {
-    Materialize.toast(data, 50000);
+socket.on("toast", data => {
+  Materialize.toast(data, 50000);
 });
 
-socket.on('enable', (extra) => {
-    $(extra).removeClass('disabled');
+socket.on("enable", extra => {
+  $(extra).removeClass("disabled");
 });
