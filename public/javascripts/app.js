7e9efb24b24d29ad5aefb5d08c89ea706d70baa2
vmarlier <valentin.marlier@gmail.com>
2018-03-02 13:15:39 +0100

    [Added] Song on extra click

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,51 +1,98 @@
+/* jshint esversion: 6*/
 const socket = io();
+// YesSong
+const maggie = new Audio("https://www.memoclic.com/medias/sons-wav/0/8.mp3");
+const bart = new Audio("");
+const lisa = new Audio("");
+const marge = new Audio("");
+const homer = new Audio("https://www.memoclic.com/medias/sons-wav/0/8.mp3");
+// NoSong
+const NoMaggie = new Audio("");
+const NoBart = new Audio("");
+const NoLisa = new Audio("");
+const NoMarge = new Audio("https://www.memoclic.com/medias/sons-wav/1/404.mp3");
+const NoHomer = new Audio("http://www.soundboard.com/sb/sound/97830");
 
-socket.on('init', (game) => {
-    $('.nbDonuts').text(game.donuts);
-    $('.donutsPerSec').text(game.donutsPerS);
-    for (i = 1; i < 6; i++) {
-        $('.extra' + i + ' .extra-counter span').text(game.extra[i].count);
-    }
-});
-
-$('#donutLink').click(() => {
-    socket.emit('addDonut', true);
-    $('#donutLink img').toggleClass('transition');
-});
-
-$('.extra1').click(() => {
-    socket.emit('addExtra', 1);
-});
-$('.extra2').click(() => {
-    socket.emit('addExtra', 2);
-});
-$('.extra3').click(() => {
-    socket.emit('addExtra', 3);
-});
-$('.extra4').click(() => {
-    socket.emit('addExtra', 4);
-});
-$('.extra5').click(() => {
-    socket.emit('addExtra', 5);
+socket.on("init", game => {
+  $(".nbDonuts").text(game.donuts);
+  $(".donutsPerSec").text(game.donutsPerS);
+  for (i = 1; i < 6; i++) {
+    $(".extra" + i + " .extra-counter span").text(game.extra[i].count);
+  }
 });
 
-socket.on('getDonuts', function (data) {
-    $('.nbDonuts').text(data);
-    updateAchievement();
+$("#donutLink").click(() => {
+  socket.emit("addDonut", true);
+  $("#donutLink img").toggleClass("transition");
 });
 
-socket.on('toast', (data) => {
-    Materialize.toast(data, 1000);
+$(".extra1").click(() => {
+  socket.emit("addExtra", 1);
+});
+$(".extra2").click(() => {
+  socket.emit("addExtra", 2);
+});
+$(".extra3").click(() => {
+  socket.emit("addExtra", 3);
+});
+$(".extra4").click(() => {
+  socket.emit("addExtra", 4);
+});
+$(".extra5").click(() => {
+  socket.emit("addExtra", 5);
 });
 
-socket.on('enable', (extra) => {
-    if (extra !== null) {
-        $(extra).removeClass('disabled');
-    }
+socket.on("getDonuts", function(data) {
+  $(".nbDonuts").text(data);
+  updateAchievement();
 });
 
-socket.on('getExtra', (extra, count, donuts, donutsPerSec) => {
-    $('.extra' + extra + ' .extra-counter span').text(count);
-    $('.nbDonuts').text(donuts);
-    $('.donutsPerSec').text(donutsPerSec);
+socket.on("toast", data => {
+  Materialize.toast(data, 1000);
+});
+
+socket.on("enable", extra => {
+  if (extra !== null) {
+    $(extra).removeClass("disabled");
+  }
+});
+
+socket.on("getExtra", (extra, count, donuts, donutsPerSec) => {
+  $(".extra" + extra + " .extra-counter span").text(count);
+  $(".nbDonuts").text(donuts);
+  $(".donutsPerSec").text(donutsPerSec);
+});
+
+socket.on("playYesSong", extra => {
+  switch (extra) {
+    case 1:
+      lisa.play();
+      break;
+    case 2:
+      break;
+    case 3:
+      break;
+    case 4:
+      break;
+    case 5:
+      homer.play();
+      break;
+  }
+});
+
+socket.on("playNoSong", extra => {
+  switch (extra) {
+    case 1:
+      break;
+    case 2:
+      break;
+    case 3:
+      break;
+    case 4:
+      NoMarge.play();
+      break;
+    case 5:
+      NoHomer.play();
+      break;
+  }
 });
--- sockets/base.js
+++ sockets/base.js
@@ -1,159 +1,166 @@
-module.exports = function (io) {
-    io.on('connection', function (socket) {
-
-        const game = {
-            donuts: 0,
-            donutsPerS: 0,
-            donutsTot: 0,
-            extra: {
-                1: {
-                    enable: false,
-                    name: 'Maggie',
-                    count: 0,
-                    cost: 10,
-                    bonus: {
-                        donutsPerSec: 5,
-                    },
-                },
-                2: {
-                    enable: false,
-                    name: 'Lisa',
-                    count: 0,
-                    cost: 200,
-                    bonus: {
-                        donutsPerSec: 20,
-                    },
-                },
-                3: {
-                    enable: false,
-                    name: 'Bart',
-                    count: 0,
-                    cost: 3000,
-                    bonus: {
-                        donutsPerSec: 40,
-                    },
-                },
-                4: {
-                    enable: false,
-                    name: 'Marge',
-                    count: 0,
-                    cost: 40000,
-                    bonus: {
-                        donutsPerSec: 500,
-                    },
-                },
-                5: {
-                    enable: false,
-                    name: 'Homer',
-                    count: 0,
-                    cost: 500000,
-                    bonus: {
-                        donutsPerSec: 2000,
-                    },
-                },
-            },
-            achievements: [{
-                    name: 'Maggie débloqué',
-                    enable: false,
-                    isUnlock: () => {
-                        if (game.donutsTot >= 10) {
-                            return true;
-                        }
-                        return false;
-                    },
-                    unlock: '.extra1',
-                },
-                {
-                    name: 'Lisa débloqué !',
-                    enable: false,
-                    isUnlock: () => {
-                        if (game.donutsTot >= 200) {
-                            return true;
-                        }
-                        return false;
-                    },
-                    unlock: '.extra2',
-                },
-                {
-                    name: 'Bart débloqué !',
-                    enable: false,
-                    isUnlock: () => {
-                        if (game.donutsTot >= 3000) {
-                            return true;
-                        }
-                        return false;
-                    },
-                    unlock: '.extra3',
-                },
-                {
-                    name: 'Marge débloqué !',
-                    enable: false,
-                    isUnlock: () => {
-                        if (game.donutsTot >= 40000) {
-                            return true;
-                        }
-                        return false;
-                    },
-                    unlock: '.extra4',
-                },
-                {
-                    name: 'Homer débloqué !',
-                    enable: false,
-                    isUnlock: () => {
-                        if (game.donutsTot >= 500000) {
-                            return true;
-                        }
-                        return false;
-                    },
-                    unlock: '.extra5',
-                },
-
-            ],
-        };
-
-        socket.emit('init', game);
-
-        socket.on('addDonut', (data) => {
-            game.donutsTot += 1;
-            game.donuts += 1;
-            achievements();
-            socket.emit('getDonuts', game.donuts);
-        });
-
-        function achievements() {
-            game.achievements.forEach(achievement => {
-                if (!achievement.enable) {
-                    achievement.enable = achievement.isUnlock();
-                    if (achievement.enable) {
-                        socket.emit('toast', achievement.name);
-                        socket.emit('enable', achievement.unlock);
-                    }
-                }
-            });
+module.exports = function(io) {
+  io.on("connection", function(socket) {
+    const game = {
+      donuts: 0,
+      donutsPerS: 0,
+      donutsTot: 0,
+      extra: {
+        1: {
+          enable: false,
+          name: "Maggie",
+          count: 0,
+          cost: 10,
+          bonus: {
+            donutsPerSec: 5
+          }
+        },
+        2: {
+          enable: false,
+          name: "Lisa",
+          count: 0,
+          cost: 200,
+          bonus: {
+            donutsPerSec: 20
+          }
+        },
+        3: {
+          enable: false,
+          name: "Bart",
+          count: 0,
+          cost: 3000,
+          bonus: {
+            donutsPerSec: 40
+          }
+        },
+        4: {
+          enable: false,
+          name: "Marge",
+          count: 0,
+          cost: 40000,
+          bonus: {
+            donutsPerSec: 500
+          }
+        },
+        5: {
+          enable: false,
+          name: "Homer",
+          count: 0,
+          cost: 500000,
+          bonus: {
+            donutsPerSec: 2000
+          }
         }
-
-        function donutsPerSec() {
-            game.donuts += game.donutsPerS;
-            game.donutsTot += game.donutsPerS;
-            achievements();
-            socket.emit('getDonuts', game.donuts);
+      },
+      achievements: [
+        {
+          name: "Maggie débloqué",
+          enable: false,
+          isUnlock: () => {
+            if (game.donutsTot >= 10) {
+              return true;
+            }
+            return false;
+          },
+          unlock: ".extra1"
+        },
+        {
+          name: "Lisa débloqué !",
+          enable: false,
+          isUnlock: () => {
+            if (game.donutsTot >= 200) {
+              return true;
+            }
+            return false;
+          },
+          unlock: ".extra2"
+        },
+        {
+          name: "Bart débloqué !",
+          enable: false,
+          isUnlock: () => {
+            if (game.donutsTot >= 3000) {
+              return true;
+            }
+            return false;
+          },
+          unlock: ".extra3"
+        },
+        {
+          name: "Marge débloqué !",
+          enable: false,
+          isUnlock: () => {
+            if (game.donutsTot >= 40000) {
+              return true;
+            }
+            return false;
+          },
+          unlock: ".extra4"
+        },
+        {
+          name: "Homer débloqué !",
+          enable: false,
+          isUnlock: () => {
+            if (game.donutsTot >= 500000) {
+              return true;
+            }
+            return false;
+          },
+          unlock: ".extra5"
         }
+      ]
+    };
 
-        setInterval(donutsPerSec, 1000);
+    socket.emit("init", game);
 
-        socket.on('addExtra', (extra) => {
-            if (game.donuts > game.extra[extra].cost) {
-                game.extra[extra].count++;
-                game.donuts -= game.extra[extra].cost;
-                game.donutsPerS += game.extra[extra].bonus.donutsPerSec;
-                socket.emit('getExtra', extra, game.extra[extra].count, game.donuts, game.donutsPerS);
-            } else {
-                socket.emit('toast', 'Donuts insuffisant');
-            }    
-        });
-
-        socket.on('disconnect', function () {
-            // Voir ce qu'on fait 
-        });
+    socket.on("addDonut", data => {
+      game.donutsTot += 1;
+      game.donuts += 1;
+      achievements();
+      socket.emit("getDonuts", game.donuts);
     });
+
+    function achievements() {
+      game.achievements.forEach(achievement => {
+        if (!achievement.enable) {
+          achievement.enable = achievement.isUnlock();
+          if (achievement.enable) {
+            socket.emit("toast", achievement.name);
+            socket.emit("enable", achievement.unlock);
+          }
+        }
+      });
+    }
+
+    function donutsPerSec() {
+      game.donuts += game.donutsPerS;
+      game.donutsTot += game.donutsPerS;
+      achievements();
+      socket.emit("getDonuts", game.donuts);
+    }
+
+    setInterval(donutsPerSec, 1000);
+
+    socket.on("addExtra", extra => {
+      if (game.donuts > game.extra[extra].cost) {
+        game.extra[extra].count++;
+        game.donuts -= game.extra[extra].cost;
+        game.donutsPerS += game.extra[extra].bonus.donutsPerSec;
+        socket.emit(
+          "getExtra",
+          extra,
+          game.extra[extra].count,
+          game.donuts,
+          game.donutsPerS
+        );
+        socket.emit("playYesSong", extra);
+      } else {
+        socket.emit("playNoSong", extra);
+        socket.emit("toast", "Donuts insuffisant");
+      }
+    });
+
+    socket.on("disconnect", function() {
+      // Voir ce qu'on fait
+    });
+  });
 };
