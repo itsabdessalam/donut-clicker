f8bc4bd7e18d1e311f62244a4340ea5fe820e1e3
Abdessalam98 <belmehelabdessalam.benharira@ynov.com>
2018-03-22 10:47:36 +0100

    [Merge] Laurent part

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -77,6 +77,7 @@ $('.switchVolume').change(function (evt) {
 socket.on('init', (game) => {
   console.log('Init Game...');
   $('.nbDonuts').text(beautifyNumber(game.donuts));
+  $('title').text(beautifyNumber(game.donuts) + ' donuts - Donut Clicker');
   $('.donutsPerSec').text(beautifyNumber(game.donutsPerS));
   $('.donutParticleNb').text('+' + beautifyNumber(game.donutsPerC, true));
   for (i = 1; i < 6; i++) {
@@ -99,6 +100,12 @@ $("#donutLink").click(() => {
   // $("#donutLink img").toggleClass("transition");
 });
 
+// rempplacer par la classe correspondante au boutton radio
+// voir a utiliser 'onchange' 
+$('.multiplier').click(() => {
+  socket.emit('updateBuy', $('.multiplier').val());
+});
+
 $(".extra1").click(() => {
   socket.emit("addExtra", 1);
 });
@@ -117,6 +124,7 @@ $(".extra5").click(() => {
 
 socket.on('getDonuts', function (data) {
   $('.nbDonuts').text(beautifyNumber(data));
+  $('title').text(beautifyNumber(data) + ' donuts - Donut Clicker');
 });
 
 socket.on("toast", data => {
